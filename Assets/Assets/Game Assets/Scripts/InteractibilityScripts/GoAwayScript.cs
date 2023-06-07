631dd0a09359e6d440c02a72cd4ac737f957db6b
KurtSlyCamilleri <104427355+KurtSlyCamilleri@users.noreply.github.com>
2023-06-08 00:45:44 +0200

    Grass implemented, player proximity loading implemented.
    Grass implemented, player proximity loading implemented.

--- Assets/Assets/Game Assets/Scripts/InteractibilityScripts/GoAwayScript.cs
+++ Assets/Assets/Game Assets/Scripts/InteractibilityScripts/GoAwayScript.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class GoAwayScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Part1;
+    public GameObject Part2;
+    public GameObject Part3;
+    public GameObject Part4;
+    public GameObject Part5;
+    public GameObject Part6;
+    public GameObject Part7;
+    public GameObject Part8;
+    public GameObject Part9;
+    
+        // Start is called before the first frame update
     void Start()
     {
         
@@ -14,5 +24,36 @@ public class GoAwayScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Camera")
+        {
+            Part1.SetActive(false);
+            Part2.SetActive(false);
+            Part3.SetActive(false);
+            Part4.SetActive(false);
+            Part5.SetActive(false);
+            Part6.SetActive(false);
+            Part7.SetActive(false);
+            Part8.SetActive(false);
+            Part9.SetActive(false);
+        }
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Camera")
+        {
+            Part1.SetActive(true);
+            Part2.SetActive(true);
+            Part3.SetActive(true);
+            Part4.SetActive(true);
+            Part5.SetActive(true);
+            Part6.SetActive(true);
+            Part7.SetActive(true);
+            Part8.SetActive(true);
+            Part9.SetActive(true);
+        }
     }
 }
